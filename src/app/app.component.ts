eaaf94097b4e0989c7b6c3342a0dfce66529f3cf
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-07 11:37:57 +0000

    fetch posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { Post } from './Post';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +12,31 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    this.http.post('https://services-revision.firebaseio.com/posts.json', postData)
-    .subscribe(responseData=>{
-      console.log(responseData);
-    })
+    this.http.post<{ name: string }>('https://services-revision.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      })
   }
 
   onFetchPosts() {
     // Send Http request
+   this.http.get<{ [key: string]: Post }>('https://services-revision.firebaseio.com/posts.json')
+      .pipe(map(resp => {
+        return Object.keys(resp).map(id => ({ ...resp[id], id }))
+      }))
+      .subscribe(posts => {
+        console.log(posts)
+        this.loadedPosts = posts;
+      })
   }
 
   onClearPosts() {
