ce49d2b8e6969025b93d54daefab1a1479b07272
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-08 18:47:57 +0000

    add interceptor

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,11 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const updReq = req.clone({
+      headers: req.headers.append('auth', 'basic')
+    });
+
+    return next.handle(updReq);
+  }
+}
