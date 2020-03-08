437933f2775365df83209dbf5f805bd71a8876b5
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2020-03-08 21:20:04 +0000

    add httpheader in readme file

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,9 +2,7 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const updReq = req.clone({
-      headers: req.headers.append('auth', 'basic')
-    });
+    const updReq = req.clone({ headers: req.headers.append('auth', 'basic') });
 
     return next.handle(updReq);
   }
